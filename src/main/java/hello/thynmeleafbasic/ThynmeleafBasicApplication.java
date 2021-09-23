a844b0b78be9f7e8634d85039398fa1564f5772b
Jaehyeong Ha <cubeprince@gmail.com>
2021-09-23 20:42:27 +0900

    project set up

---
+++ src/main/java/hello/thynmeleafbasic/ThynmeleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thynmeleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThynmeleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThynmeleafBasicApplication.class, args);
+	}
+
+}
